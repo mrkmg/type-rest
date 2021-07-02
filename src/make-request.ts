e425669fb7940aa2891b0c4702200ffa403447ff
Kevin Gravier <kevin@mrkmg.com>
2021-07-02 01:15:17 -0400

    Cleanup: Reorganized code to make more sense

--- src/make-request.ts
+++ src/make-request.ts
@@ -4,14 +4,14 @@ import {TypeRestDefaults} from "./type-rest";
 
 export function makeRequest<T>(preHookEvent: IPreHookEvent<T>): Promise<Response> {
     const params: RequestInit = Object.assign({}, preHookEvent.options.params);
-
-    let url = preHookEvent.uri;
-
     params.method = preHookEvent.method;
+
     if (preHookEvent.requestBody !== null) {
         params.body = JSON.stringify(preHookEvent.requestBody);
         params.headers["Content-Type"] = "application/json";
     }
+
+    let url = preHookEvent.uri;
 
     if (preHookEvent.requestQuery !== null) {
         url = url + "?" + buildQueryString(preHookEvent.requestQuery);

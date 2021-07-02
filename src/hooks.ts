b903c73153c50858ee2d46ee54166be55519358e
Kevin Gravier <kevin@mrkmg.com>
2021-07-02 01:14:33 -0400

    Cleanup: Made a little more efficient

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,41 +1,26 @@
 import {Index, ITypeRestOptions, UntypedTypeRestApi} from "./index";
 import {IEndPointParams} from "./make-endpoint";
 
+type HookDefinition = Pick<IPreHookDefinition, "path" | "method">;
+type EventDefinition = Pick<IPreHookEvent<unknown>, "path" | "method">;
+function hookMatchesEvent(hookDefinition: HookDefinition, event: EventDefinition) {
+    return (!hookDefinition.path || hookDefinition.path === event.path) &&
+        (!hookDefinition.method || hookDefinition.method === event.method);
+}
+
 export async function runPreHooks<T>(params: IEndPointParams<T>, preEvent: IPreHookEvent<T>): Promise<void> {
-    const matchingHooks = params.current._fullOptions.hooks.filter((hookDefinition: IHookDefinition<T>) => {
-        if (hookDefinition.type === "post") {
-            return false;
-        }
-        const isInvalidPath = hookDefinition.path && hookDefinition.path !== preEvent.path;
-        const isInvalidMethod = hookDefinition.method && hookDefinition.method !== preEvent.method;
-        return !(isInvalidPath || isInvalidMethod);
-    }) as IPreHookDefinition<T>[];
-
-    if (matchingHooks.length === 0) {
-        return Promise.resolve();
-    }
-
-    for (const hook of matchingHooks) {
-        await hook.hook(preEvent);
+    for (const hookDefinition of params.current._fullOptions.hooks) {
+        if (hookDefinition.type !== "pre" || !hookMatchesEvent(hookDefinition, preEvent))
+            continue;
+        await hookDefinition.hook(preEvent);
     }
 }
 
 export async function runPostHooks<T>(params: IEndPointParams<T>, postEvent: IPostHookEvent<T>): Promise<void> {
-    const matchingHooks = params.current._fullOptions.hooks.filter((hookDefinition: IHookDefinition<T>) => {
-        if (hookDefinition.type === "pre") {
-            return false;
-        }
-        const isInvalidPath = hookDefinition.path && hookDefinition.path !== postEvent.path;
-        const isInvalidMethod = hookDefinition.method && hookDefinition.method !== postEvent.method;
-        return !(isInvalidPath || isInvalidMethod);
-    }) as IPostHookDefinition<T>[];
-
-    if (matchingHooks.length === 0) {
-        return Promise.resolve();
-    }
-
-    for (const hook of matchingHooks) {
-        await hook.hook(postEvent);
+    for (const hookDefinition of params.current._fullOptions.hooks) {
+        if (hookDefinition.type !== "post" || !hookMatchesEvent(hookDefinition, postEvent))
+            continue;
+        await hookDefinition.hook(postEvent);
     }
 }
 
